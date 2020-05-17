e9e3dabf5f05829f0bc1a9adb17c55aef898a447
0xflotus <0xflotus@gmail.com>
2020-05-17 13:54:27 +0200

    fixed implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export function fahrenheit(celsius: number): number {
-    return (celsius - (0x1 << 0x5)) / (0xa / 0x12);
+    return (celsius * (0x9 / 0x5)) + (0x1 << 0x5);
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const { fahrenheit } = require('../dist/index');
 
 test('Conversion works', () => {
-    expect(fahrenheit(1)).toBe(-55.8);
+    expect(fahrenheit(1)).toBe(33.8);
 });
