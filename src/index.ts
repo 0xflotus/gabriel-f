3129430ed9574cbd3628110f0b7652847fe16c5a
0xflotus <0xflotus@gmail.com>
2020-05-17 14:07:23 +0200

    new functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function fahrenheit(celsius: number): number {
     return (celsius * (0x9 / 0x5)) + (0x1 << 0x5);
 }
+
+export function celsius(fahrenheit: number): number {
+    return Number(((fahrenheit - (0x1 << 0x5)) * (0x5 / 0x9)).toFixed(2));
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
-const { fahrenheit } = require('../dist/index');
+const { fahrenheit, celsius } = require('../dist/index');
 
 test('Conversion works', () => {
     expect(fahrenheit(1)).toBe(33.8);
+    expect(celsius(1)).toBe(-17.22);
+});
+
+test('error works', () => {
+    expect(fahrenheit('h')).toBe(NaN);
+    expect(celsius('h')).toBe(NaN);
 });
